305bddfd1a465188675ccc883a3ee7034b8f4521
@fausto1 <fausto@parts-badger.com>
2021-05-12 17:04:06 -0500

    includ build

---
+++ build/precache-manifest.e74efad72cdebfc003560025bff08dff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a23c0cacff0c6cd55f12834ad71f69",
+    "url": "/react-camera-pro/index.html"
+  },
+  {
+    "revision": "b3bf46bbbbaf34bc38a8",
+    "url": "/react-camera-pro/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "355dba9d9b0214c15c1e",
+    "url": "/react-camera-pro/static/js/2.13cc4212.chunk.js"
+  },
+  {
+    "revision": "b3bf46bbbbaf34bc38a8",
+    "url": "/react-camera-pro/static/js/main.c430fa15.chunk.js"
+  },
+  {
+    "revision": "8a6545c21b1c4afd00e7",
+    "url": "/react-camera-pro/static/js/runtime-main.ed64f0d0.js"
+  }
+]);
